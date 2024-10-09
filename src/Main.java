b1179820314ee0087e990e827be2eaf0773bb679
AnjaMajurec <amajurec277@gmail.com>
2024-10-09 09:32:22 +0200

    Provjera commita

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Anja Majurec");
         System.out.println("Treći dan predavanja!");
+        System.out.println("Četvrti dan predavanja!");
 
     }
 }
