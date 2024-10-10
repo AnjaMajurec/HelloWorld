509b5ee0c8f0c50dcc64b8bc65617db7e54e142e
AnjaMajurec <amajurec277@gmail.com>
2024-10-10 20:23:49 +0200

    Zadatak za vježbu-pogodi broj.

--- src/Main.java
+++ src/Main.java
@@ -7,37 +7,18 @@ import java.util.Scanner;
 
                 Scanner scanner = new Scanner(System.in);
 
-                System.out.println("KALKULATOR!");
+                System.out.println("Pogađanje tajnog broja!");
 
-                System.out.println("Molim Vas unesite prvi broj:");
+                int tajniBroj = 7;
+                int pogodak;
 
-                double uneseniBroj1 = scanner.nextDouble();
+                do{
+                    System.out.println("Pogodi broj: ");
+                    pogodak = scanner.nextInt();
 
-                System.out.println("Molim Vas unesite drugi broj:");
-
-                double uneseniBroj2 = scanner.nextDouble();
-
-                System.out.println("Molim Vas unesite matematičku radnju koju želite izvršiti (1-zbrajanje, 2-oduzimanje, 3-množenje, 4-dijeljenje):");
-
-                int radnja = scanner.nextInt();
-
-                if (radnja == 1){
-                    double rezultat = uneseniBroj1 + uneseniBroj2;
-                    System.out.println("Korisnik je izabrao zbrajanje dva unesena broja " + uneseniBroj1 + " i " + uneseniBroj2 + " te je rezultat jednak: " + rezultat + ".");
                 }
-
-                else if (radnja == 2){
-                    double rezultat = uneseniBroj1 - uneseniBroj2;
-                    System.out.println("Korisnik je izabrao oduzimanje dva unesena broja " + uneseniBroj1 + " i " + uneseniBroj2 + "  te je rezultat jednak: " + rezultat + ".");
-                }
-                else if (radnja == 3){
-                    double rezultat = uneseniBroj1 * uneseniBroj2;
-                    System.out.println("Korisnik je izabrao množenje dva unesena broja " + uneseniBroj1 + " i " + uneseniBroj2 + "  te je rezultat jednak: " + rezultat + ".");
-                }
-                else {
-                    double rezultat = uneseniBroj1 / uneseniBroj2;
-                    System.out.println("Korisnik je izabrao dijeljenje dva unesena broja " + uneseniBroj1 + " i " + uneseniBroj2 + "  te je rezultat jednak: " + rezultat + ".");
-                }
+                while (pogodak > tajniBroj || pogodak < tajniBroj);
+                System.out.println("Bravo, uspješno ste pogodili tajni broj 7.");
 
             }
         }
