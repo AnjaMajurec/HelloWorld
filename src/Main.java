55647bc8365b2adb7ef1257ceb4b70b42e21bc5d
AnjaMajurec <amajurec277@gmail.com>
2024-10-12 15:58:26 +0200

    Zadatak 1

--- src/Main.java
+++ src/Main.java
@@ -7,18 +7,19 @@ import java.util.Scanner;
 
                 Scanner scanner = new Scanner(System.in);
 
-                System.out.println("Zbrajanje");
+                //ZADATAK 1
+                //Napišite program koji će izračunati sumu svih brojeva od 1 do N, gdje je N pozitivan cijeli broj unesen od strane korisnika.
 
+                System.out.println("Unesite jedan cijeli broj: ");
+                int broj = scanner.nextInt();
                 int zbroj = 0;
-                int unos = 1;
 
-                while (unos != 0){
-                    System.out.println("Unesi broj, za kraj programa unesi 0.");
-                    unos = scanner.nextInt();
-                    zbroj+=unos;
+                for (int i=1; i<(broj+1);i++){
+                    zbroj+=i;
                 }
+                System.out.println("Zbroj brojeva od 1 do " + broj + " iznosi " + zbroj + ".");
 
-                System.out.println("Zbroj prethodno unesenih brojeva je: " + zbroj);
+
 
             }
         }
