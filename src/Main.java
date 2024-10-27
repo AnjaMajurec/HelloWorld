8ad424fff0527c302bf33e49d4e49200db253ffb
AnjaMajurec <amajurec277@gmail.com>
2024-10-27 10:56:07 +0100

    Drugi zadatak, vikend 27.-28.10.

--- src/Main.java
+++ src/Main.java
@@ -3,59 +3,72 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Main {
-
-    // Prvi zadatak
-    // Statistika riječi Napravi program koji učitava rečenicu i ispisuje:
-    // 1. Sve riječi duže od 5 slova, 2. Najdužu riječ, 3.Prosječnu duljinu riječi.
+    // Zadatak 2
+    // Telefonski imenik:
+    // Napravi program koji simulira telefonski imenik koristeći dvije paralelne liste:
+    // Lista u koju se spremaju imena, i lista u kojoj se sprema brojevi
+    // Kod prvog odabira korisnik unosi ime i broj koji se spremaju u svoje liste
+    // Korisnik ima mogucnost odabira, te za svaki odabir treba omoguciti odredenu funkcionalnost sa slike.
 
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Unesite željenu rečenicu: ");
-        String recenica = scanner.nextLine();
+        List<String> ime = new ArrayList<>();
+        List<String> broj = new ArrayList<>();
 
-        String[] rijeci = recenica.split(" ");
-
-        List<String> lista = new ArrayList<>();
-        for (String s : rijeci) {
-            lista.add(s);
-        }
-        petSlova(lista);
-        najduzaRijec(lista);
-        System.out.println("Prosječna duljina riječi je: " + prosjecnaDuljinaRijeci(lista));
+        System.out.println("TELEFONSKI IMENIK");
+        int radnja = 1;
+        do {
+            if (radnja == 1) {
+                System.out.println("Unesi korisnikovo ime: ");
+                ime.add(scanner.nextLine());
+                System.out.println("Unesi korisnikov broj telefona: ");
+                broj.add(scanner.nextLine());
+            }
+            else if (radnja == 2) {
+                pretraziPoImenu(ime, broj);
+            }
+            else if (radnja == 3) {
+                pretraziPoBroju(ime,broj);
+            }
+            else if (radnja == 4) {
+                ispisiSve(ime,broj);
+            }
+            System.out.println("Odaberite radnju koju želite izvršiti brojevima 1-5: ");
+            System.out.println("1 - Dodaj kontakt, 2 - Pretraži po imenu, 3 - Pretraži po broju, 4 - Ispiši sve, 5 - Izlaz");
+            radnja = scanner.nextInt();
+            scanner.nextLine();
+        } while (radnja < 5);
+        System.out.println("Izašli ste iz telefonskog imenika.");
 
     }
-    public static void petSlova(List<String> lista){
-        System.out.print("Riječi koje imaju više od 5 slova: ");
-        for(int i=0;i<lista.size();i++){
-            if(lista.get(i).length()>5){
-                System.out.print(lista.get(i)+" ");
-            }
-        }
-        System.out.println();
-    }
-    public static void najduzaRijec (List<String> lista){
-        String rijecMax=" ";
-        for(int i=0;i<lista.size();i++){
-            if(lista.get(i).length()>rijecMax.length()){
-                rijecMax=lista.get(i);
-            }
-        }
-        //ako imamo više riječi koje imaju isto slova ko najduža riječ
-        System.out.print("Najduža riječ je: ");
-        for(int i=0;i<lista.size();i++) {
-            if (lista.get(i).length() == rijecMax.length()) {
-                System.out.print(lista.get(i)+" ");
-            }
-        }
-        System.out.println();
-    }
 
-    public static Integer prosjecnaDuljinaRijeci (List<String> lista){
-        int ukupnaDuljina=0;
-        for(int i=0;i<lista.size();i++){
-            ukupnaDuljina+=lista.get(i).length();
+    public static void pretraziPoImenu(List<String> ime, List<String> broj) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Unesite korisnikovo ime kako biste dobili njegov broj: ");
+        String korisnik = scanner.nextLine();
+        for (int i = 0; i < ime.size(); i++) {
+            if (ime.get(i).equals(korisnik)) {
+                System.out.println("Korisnikov broj je: " + broj.get(i));
+                return;
+            }
         }
-        return ukupnaDuljina/lista.size();
+    }
+    public static void pretraziPoBroju(List<String> ime, List<String> broj) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Unesite korisnikov broj kako biste dobili njegovo ime: ");
+        String pretragaBroj = scanner.nextLine();
+        for (int i = 0; i < ime.size(); i++) {
+            if (broj.get(i).equals(pretragaBroj)) {
+                System.out.println("Korisnikovo ime je: " + ime.get(i));
+                return;
+            }
+        }
+    }
+    public static void ispisiSve (List <String> ime, List <String> broj) {
+        System.out.println("Imena i brojevi korisnika iz telefonskog imenika: ");
+        for(int i=0; i<ime.size();i++){
+            System.out.println(ime.get(i)+" "+broj.get(i));
+        }
     }
 }
