7724ac376e0456e37f4dac236f114cd40359a9c7
AnjaMajurec <amajurec277@gmail.com>
2024-11-13 14:53:05 +0100

    DOdatno-abstract klasa

---
+++ src/AbstraktnaKlasaKnjiga.java
@@ -0,0 +1,63 @@
+import java.time.Year;
+
+public class AbstraktnaKlasaKnjiga implements Knjiga {
+    private final String naslov;
+    private final String autor;
+    private String stanje;
+    private Integer godinaIzdavanja;
+    private Integer brojStranica;
+
+    public AbstraktnaKlasaKnjiga(String naslov, String autor, Integer godinaIzdavanja, Integer brojStranica) {
+        this.naslov = naslov;
+        this.autor = autor;
+        if (godinaIzdavanja > 1900 && godinaIzdavanja <= Year.now().getValue()) {
+            this.godinaIzdavanja = godinaIzdavanja;
+        } else {
+            System.out.println("Godina izdavanja knjige mora biti između 1900. godine i trenutne godine.");
+        }
+        if (brojStranica > 0) {
+            this.brojStranica = brojStranica;
+        } else {
+            System.out.println("Krivi unos broja stranica.");
+        }
+
+        this.stanje = "Dostupno";
+    }
+
+    public String getNaslov() {
+        return naslov;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public Integer getGodinaIzdavanja() {
+        return godinaIzdavanja;
+    }
+
+
+    public Integer getBrojStranica() {
+        return brojStranica;
+    }
+
+    public String getStanje() {
+        return stanje;
+    }
+
+    public void posudi() {
+        if (stanje.equals("Dostupno")) {
+            this.stanje = "Posuđeno";
+        } else {
+            System.out.println("Knjiga je već posuđena.");
+        }
+    }
+
+    public void vratiti() {
+        if (stanje.equals("Posuđeno")) {
+            this.stanje = "Dostupno";
+        } else {
+            System.out.println("Knjiga je dostupna.");
+        }
+    }
+}
