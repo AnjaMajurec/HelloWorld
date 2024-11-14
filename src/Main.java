3e35ab702c3ba5de0dd06beb73409e1dc212885e
AnjaMajurec <amajurec277@gmail.com>
2024-11-14 13:20:16 +0100

    Zadatak od jučer, 13.11.

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,54 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
-    //Definicija interface Knjiga s metodama:
-    //getNaslov(): vraća naslov knjige
-    //getAutor(): vraća autora knjige
-    //getGodinaIzdavanja(): vraća godinu izdavanja knjige
-    //getBrojStranica(): vraća broj stranica knjige
-    //getStanje(): vraća stanje knjige (npr. "Dostupno" ili "Posuđeno")
-    //posudi(): označava knjigu kao posuđenu
-    //vrati(): označava knjigu kao dostupnu
-    //Implementacija klase Roman i Strip koje implementiraju interface Knjiga
-    //Zahtjevi:
-    //Upotrijebite interface Knjiga za definiranje zajedničkih metoda
-    //Implementirajte klase Roman i Strip koje će sadržavati privatne varijable za naslov, autora, godinu izdavanja, broj stranica i stanje knjige.
-    //Provjerite valjanost unosa godine izdavanja (trebala bi biti između 1900 i trenutne godine)
-    //Provjerite valjanost unosa broja stranica (trebao bi biti pozitivan broj)
-    //Implementirajte metode posudi() i vrati() tako da ažuriraju stanje knjige
-    //Unutar main klase ne morate nista raditi, bitno je da su sve ove metode napisane u klasama i interfaceu.
-    //DODATNO: napraviti abstract klasu knjiga i implementirati u zadatak prema pravilima onako kako smo radili na predavanju.
+    //Napiši Java program koji omogućuje korisniku unos broja, a zatim računa faktorijel tog broja.
+    //Program treba obraditi iznimke koje se mogu pojaviti tijekom unosa i računanja faktorijela.
+    //Ispiši poruku korisniku da unese broj.
+    // Koristi Scanner za unos broja od korisnika.
+    // Obradi iznimke koje se mogu pojaviti tijekom unosa. Ako korisnik unese nešto što nije broj,
+    // ispiši odgovarajuću poruku i zatraži ponovni unos.
+    // Računaj faktorijel unesenog broja.(funkcija throws Exception)
+    // Obradi iznimke koje se mogu pojaviti tijekom računanja faktorijela. Na primjer,
+    // obradi situaciju kada korisnik unese negativan broj jer faktorijel nije definiran za negativne brojeve.
+    // Ispiši rezultat ili poruku o greški, ovisno o tome je li računanje faktorijela uspješno ili ne.
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean provjera = false;
+
+        do {
+            try {
+                System.out.println("Unesite broj za koji računamo faktorijel: ");
+                int broj = scanner.nextInt();
+                provjera = true;
+                System.out.println("Faktorijel broja " + broj + " iznosi " + izracunajFaktorijel(broj) + ".");
+
+
+            } catch (InputMismatchException e) {
+                System.out.println("Greška. Unesite cijeli broj: ");
+                scanner.next();
+
+            } catch (Exception e) {
+                System.out.println("Greška: " + e.getMessage());
+                provjera = false;
+
+            }
+
+        } while (!provjera);
+
+    }
+
+    public static double izracunajFaktorijel(int broj) throws Exception {
+        if (broj < 0.0) {
+            throw new Exception("Faktorijel nije definiran za negativne brojeve.");
+        } else if (broj == 0) {
+            return 1;
+        } else {
+            double faktorijel = 1;
+            for (int i = 1; i <= broj; i++) {
+                faktorijel = faktorijel * i;
+            }
+            return faktorijel;
+        }
+    }
 }
