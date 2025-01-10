445cb3af0232bb3656440e2ab72ba8366743328e
AnjaMajurec <amajurec277@gmail.com>
2025-01-10 21:34:44 +0100

    Zadatak 2, 10.1.-vikend

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,36 @@ public class Main {
     public static void main(String[] args) {
 
         HashMap<String, Integer> rijecBrojPonavljanja = new HashMap<>();
+        HashSet<String> jedinstveniBrojevi = new HashSet<>();
+
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Unesite rečenicu: ");
-        String recenica = scanner.nextLine();
+        System.out.println("Unesite brojeve odvojene zarezom: ");
+        String unosBrojeva = scanner.nextLine();
 
-        String[] rijeci = recenica.replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s+");
-        for (String rijec : rijeci) {
-            rijecBrojPonavljanja.put(rijec, rijecBrojPonavljanja.getOrDefault(rijec,0)+1);
+        String[] brojevi = unosBrojeva.split("\\s+");
+        for (String broj : brojevi) {
+            jedinstveniBrojevi.add(broj);
         }
 
-        System.out.println("Broj ponavljanja svake riječi: ");
-        System.out.println(rijecBrojPonavljanja);
+        System.out.println("Jedinstveni brojevi: ");
+        System.out.println(jedinstveniBrojevi);
+
+        int minBroj=Integer.MAX_VALUE;
+        int maxBroj=Integer.MIN_VALUE;
+
+        for (String s : jedinstveniBrojevi) {
+            int provjeraBroja=Integer.parseInt(s);
+            if(provjeraBroja<minBroj){
+                minBroj=provjeraBroja;
+            }
+            if(provjeraBroja>maxBroj){
+                maxBroj=provjeraBroja;
+            }
+        }
+        System.out.println("Najmanji broj je: "+minBroj);
+        System.out.println("Najveći broj je: "+maxBroj);
+
     }
 }
 
