de8aabdef9da5721fe66d67df49b0f6965f687f6
AnjaMajurec <amajurec277@gmail.com>
2025-01-11 17:20:18 +0100

    Zadatak 3, 11.1.-vikend

--- src/Main.java
+++ src/Main.java
@@ -3,35 +3,61 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        HashSet<String> jedinstveniBrojevi = new HashSet<>();
-
+        HashMap<String, List<Proizvod>> proizvodiGrupiraniPoKategoriji = new HashMap<>();
+        List<Proizvod> voce = new ArrayList<>();
+        List<Proizvod> povrce = new ArrayList<>();
+        proizvodiGrupiraniPoKategoriji.put("voće", voce);
+        proizvodiGrupiraniPoKategoriji.put("povrće", povrce);
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Unesite brojeve odvojene zarezom: ");
-        String unosBrojeva = scanner.nextLine();
+        System.out.println("Za unos proizvoda u kategorijama (voće, povrće) odaberite 1, za prikaz proizvoda u određenoj kategoriji 2, prikaz svih proizvoda i kategorija 3, za izlaz 4:");
+        int unos = scanner.nextInt();
+        scanner.nextLine();
 
-        String[] brojevi = unosBrojeva.split("\\s+");
-        for (String broj : brojevi) {
-            jedinstveniBrojevi.add(broj);
-        }
+        while(unos!=4){
+            if(unos==1){
+                System.out.println("U koju kategoriju želite unijeti proizvod?");
+                String unosKategorije= scanner.nextLine().toLowerCase();
+                System.out.println("Koliko proizvoda želite unijeti?");
+                Integer brojProizvoda= scanner.nextInt();
+                scanner.nextLine();
 
-        System.out.println("Jedinstveni brojevi: ");
-        System.out.println(jedinstveniBrojevi);
-
-        int minBroj=Integer.MAX_VALUE;
-        int maxBroj=Integer.MIN_VALUE;
-
-        for (String s : jedinstveniBrojevi) {
-            int provjeraBroja=Integer.parseInt(s);
-            if(provjeraBroja<minBroj){
-                minBroj=provjeraBroja;
+                for(int i=0;i<brojProizvoda;i++){
+                    System.out.println("Unesi ime proizvoda: ");
+                    String imeProizvoda = scanner.nextLine();
+                    Proizvod proizvod=new Proizvod(imeProizvoda);
+                    if(unosKategorije.equals("voće")){
+                        proizvodiGrupiraniPoKategoriji.get("voće").add(proizvod);
+                    }
+                    else if(unosKategorije.equals("povrće")){
+                        proizvodiGrupiraniPoKategoriji.get("povrće").add(proizvod);
+                    }
+                }
             }
-            if(provjeraBroja>maxBroj){
-                maxBroj=provjeraBroja;
+            else if (unos==2){
+                System.out.println("Unesite kategoriju iz koje želite prikazati proizvode (voće/povrće)");
+                String unosKategorije = scanner.nextLine().toLowerCase();
+                if(proizvodiGrupiraniPoKategoriji.containsKey(unosKategorije)){
+                    List <Proizvod> proizvodi = proizvodiGrupiraniPoKategoriji.get(unosKategorije);
+                    for (Proizvod proizvod : proizvodi) {
+                        System.out.println(proizvod);
+                    }
+                }
             }
+            else if(unos==3){
+                System.out.println("Prikaz svih proizvoda:");
+                for (String kategorija : proizvodiGrupiraniPoKategoriji.keySet()) {
+                    System.out.println("Kategorija: "+kategorija);
+                    List <Proizvod> proizvodi =proizvodiGrupiraniPoKategoriji.get(kategorija);
+                    for (Proizvod proizvod : proizvodi) {
+                        System.out.println(proizvod);
+                    }
+                }
+            }
+            System.out.println("Za unos proizvoda u kategorijama (voće, povrće) odaberite 1, za prikaz proizvoda u određenoj kategoriji 2, prikaz svih proizvoda i kategorija 3, za izlaz 4:");
+            unos = scanner.nextInt();
+            scanner.nextLine();
         }
-        System.out.println("Najmanji broj je: "+minBroj);
-        System.out.println("Najveći broj je: "+maxBroj);
-
+        System.out.println("Izašli ste iz aplikacije.");
     }
 }
 
--- src/Proizvod.java
+++ src/Proizvod.java
@@ -1,28 +1,16 @@
 public class Proizvod {
     private final String naziv;
-    private final Double cijena;
-    private final Integer kolicina;
 
-    public Proizvod(String naziv, Double cijena, Integer kolicina) {
+    public Proizvod(String naziv) {
         this.naziv = naziv;
-        this.cijena = cijena;
-        this.kolicina = kolicina;
     }
 
     public String getNaziv() {
         return this.naziv;
     }
-
-    public Double getCijena() {
-        return this.cijena;
-    }
-
-    public Integer getKolicina() {
-        return this.kolicina;
-    }
-
+    @Override
     public String toString() {
-        return naziv + ", cijena: " + cijena + ", kolicina: " + kolicina;
+        return naziv;
     }
 }
 
